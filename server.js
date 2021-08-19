b3ca65bbea368da2e1c8816f54b0fe5cd7fdcc7a
Joseph Dehoney <josephdehoney@gmail.com>
2021-08-19 14:19:18 -0700

    Generic error handling added

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 
+const { BadRequestError, NotFoundError } = require("./utils/errors")
+
 const app = express()
 
+// MIDDLEWARE
 //enables cross origin resource sharing for all origins
 app.use(cors()) 
 // parse incoming request bodies with JSON payloads
 app.use(express.json())
 // log request info
 app.use(morgan("tiny"))
+
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+})
+// Generic error handler
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status },
+    })
+})
 
 const PORT = process.env.PORT || 3001
 
